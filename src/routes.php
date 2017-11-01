2bc8ffe712594f486917dc1c030695ed5c549a20
RajGod <pedro.luiz@hotmail.com.br>
2017-11-01 15:36:56 -0200

    Inserir,pesquisar,Atualizar e deletar de remedio

--- src/routes.php
+++ src/routes.php
@@ -144,18 +144,18 @@ $app->group('/api', function () use ($app) {
             });
 
             $app->delete('/deletar/[{id_atividade}]', function ($request, $response, $args) {
-                $idAtividade=$request->getParam('id_atividade');
+                $id_atividade=$request->getParam('id_atividade');
                 try {
                     $sth = $this->db->prepare("DELETE FROM rotina WHERE id_atividade=:id_atividade");
-                    $sth->bindParam("id_atividade", $idAtividade);
+                    $sth->bindParam("id_atividade", $id_atividade);
                     $sth->execute();
             
                     $result = $sth->rowCount();
             
                     if ($result == 1) {
-                        $status = "Rotina deletada com sucesso!";
+                        $status = "Atividade deletado com sucesso!";
                     } else {
-                        $status = "Erro ao deletar a Rotina.";
+                        $status = "Erro ao deletar a Atividade.";
                     }
                     return $this->response->withJson($status);
                 } catch (Exception $e) {
@@ -167,6 +167,117 @@ $app->group('/api', function () use ($app) {
             
          });
            
+
+         $app->group('/remedio', function () use ($app) {
+            
+                        //Inserção da medicação
+                        $app->post('/inserir', function ($request, $response) {
+                            $nome=$request->getParam('nome');
+                            $idCad=$request->getParam('id_cad');
+                            $dosagem=$request->getParam('dosagem');
+                            $miligramagem=$request->getParam('miligramagem');
+                            $principio_ativo=$request->getParam('principio_ativo');
+                            $intervalo=$request->getParam('intervalo');
+                        
+                            $sql = "INSERT  medicamentos(nome,id_cad,dosagem,miligramagem,principio_ativo,intervalo) VALUES(:nome,:id_cad,:dosagem,:miligramagem,:principio_ativo,:intervalo)";
+                            try {
+                                $sth = $this->db->prepare($sql);
+                                $sth->bindParam("nome", $nome);
+                                $sth->bindParam("id_cad",$idCad);
+                                $sth->bindParam("dosagem",$dosagem);
+                                $sth->bindParam("miligramagem",$miligramagem);
+                                $sth->bindParam("principio_ativo",$principio_ativo);
+                                $sth->bindParam("intervalo",$intervalo);
+                              
+                                $sth->execute();
+                                $status = "Medicamento adicionado com sucesso!";
+                                
+                                return $this->response->withJson($status);
+                            } catch (PDOException $e) {
+                                return $this->response->withJson("Erro ao adicionar o Medicamento. " . $e->getCode(), 500);
+                            }
+                        });
+                        //selecionar lista de remedios do paciente
+                        $app->get('/pesquisar/[{id_cad}]', function ($request, $response,$args) {
+                            
+                              $idCad=$request->getParam('id_cad');
+                              $sql = "select * from medicamentos where id_cad = :id_cad";
+                              try {
+                                  $sth = $this->db->prepare($sql);
+                                  $sth->bindParam("id_cad", $idCad);
+                                 
+                  
+                                  $sth->execute();
+                                  $result = $sth->fetchAll();
+                                  return $this->response->withJson($result);
+                              } catch (PDOException $e) {
+                                  return $this->response->withJson("Erro ao Pesquisar os Medicamentos ". $e->getCode() . $e, 500);
+                              }
+                          });
+            
+                          $app->put('/atualizar', function ($request, $response, $args) {
+                            $nome=$request->getParam('nome');
+                            $id_remedio=$request->getParam('id_remedio');
+                            $dosagem=$request->getParam('dosagem');
+                            $miligramagem=$request->getParam('miligramagem');
+                            $principio_ativo=$request->getParam('principio_ativo');
+                            $intervalo=$request->getParam('intervalo');
+                           
+                        
+                            try {
+                                $sql = "UPDATE rotina SET nome=:nome, miligramagem=:miligramagem, principio_ativo=:principio_ativo , intervalo=:intervalo,dosagem=:dosagem WHERE id_remedio=:id_remedio";
+                        
+                             
+                                
+                                $sth = $this->db->prepare($sql);
+                                $sth->bindParam("nome", $nome);
+                                $sth->bindParam("id_remedio",$id_remedio);
+                                $sth->bindParam("dosagem",$dosagem);
+                                $sth->bindParam("miligramagem",$miligramagem);
+                                $sth->bindParam("principio_ativo",$principio_ativo);
+                                $sth->bindParam("intervalo",$intervalo);
+                               
+                        
+                                $sth->execute();
+                        
+                                $result = $sth->rowCount();
+                        
+                                if ($result == 1) {
+                                    $status = "Remedio atualizada com sucesso!";
+                                    return $this->response->withJson($status);
+                                } else {
+                                    $status = "Erro ao atualizar o Remedio.";
+                                    return $this->response->withJson($status);
+                                }
+                            } catch (Exception $e) {
+                                return $this->response->withJson("Ocorreu algum erro interno." . $e->getCode(), 500);
+                            }
+                        });
+            
+                        $app->delete('/deletar/[{id_remedio}]', function ($request, $response, $args) {
+                            $id_remedio=$request->getParam('id_remedio');
+                            try {
+                                $sth = $this->db->prepare("DELETE FROM medicamentos WHERE id_remedio=:id_remedio");
+                                $sth->bindParam("id_remedio", $id_remedio);
+                                $sth->execute();
+                        
+                                $result = $sth->rowCount();
+                        
+                                if ($result == 1) {
+                                    $status = "Remedio deletado com sucesso!";
+                                } else {
+                                    $status = "Erro ao deletar o remedio.";
+                                }
+                                return $this->response->withJson($status);
+                            } catch (Exception $e) {
+                                return $this->response->withJson("Ocorreu algum erro interno." . $e->getCode(), 500);
+                            }
+                        
+                        });
+                        
+                        
+                     });
+
     });
 
 });
